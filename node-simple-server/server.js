d3dd6bf9a1a452564ab8b84d2f13fad1bfe3fed4
Thiago Silva Vilela <thiagovilela@pbh.gov.br>
2019-02-12 12:11:13 -0200

    Commit inicial

---
+++ node-simple-server/server.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/home', function(req, res) {
+    console.log(req.query);
+    console.log(req.query.senha);
+    res.json("Ola mundo");
+    res.end();
+});
+
+app.post('/data', function(req, res) {
+    console.log(req.body);
+    res.end();
+});
+
+app.listen(3000);
