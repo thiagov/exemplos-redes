cbbf5f04f0b2d57facbab57058c5039e7e5472f3
Thiago Silva Vilela <thiagovilela@pbh.gov.br>
2019-02-18 15:19:01 -0300

    Adicionando exemplos

--- node-simple-server/server.js
+++ node-simple-server/server.js
@@ -1,18 +1,34 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
+var path = require('path');
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/home', function(req, res) {
+    console.log('Dados passados na URL: ');
     console.log(req.query);
-    console.log(req.query.senha);
-    res.json("Ola mundo");
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/data', function(req, res) {
+app.get('/teste_redirect', function(req, res) {
+    res.redirect(301, 'https://www.google.com');
+});
+
+app.post('/teste_post', function(req, res) {
+    console.log('Dados passados no corpo da mensagem: ');
     console.log(req.body);
-    res.end();
+    if (!req.body.nome) {
+        res.status(400);
+        res.send('Nome não fornecido!');
+    } else {
+        resposta = 'Olá ' + req.body.nome + ' ' + req.body.sobrenome;
+        res.json(resposta);
+    }
+});
+
+app.delete('/teste_post', function(req, res) {
+    res.json('Resposta de teste');
 });
 
 app.listen(3000);
